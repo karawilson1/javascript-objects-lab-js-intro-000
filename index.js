e30c62ca2f1f047db72d51ed58e7c71f6e4011e1
Wilson <karaewilson16@gmail.com>
2021-01-15 19:32:53 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,5 @@
+var recipes = new Object({ prop: 'new value', prop2: '2' })
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, recipes, {prop: '1', prop2: '2', prop: 'new value'})
+}
